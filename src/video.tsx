748e34d65092e68e1de025db94d9aacd44f669a3
Cyb3rOguh <hugo_borel@me.com>
2025-10-14 19:45:29 +0200

    liens

--- src/video.tsx
+++ src/video.tsx
@@ -20,67 +20,77 @@ const EmblaCarousel = () => {
             
                 {/* gagner */}
                 <div className="embla__slide"> 
-                    <video
-                    className="embla__video"
-                    muted
-                    loop
-                    autoPlay
-                    >
-                    <source src="/videos/gagner.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
+                    <a href="https://youtu.be/Ac3oQSA2Gv0" target="_blank" rel="noopener noreferrer">
+                        <video
+                        className="embla__video"
+                        muted
+                        loop
+                        autoPlay
+                        >
+                        <source src="/videos/gagner.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                        </video>
+                    </a>
                 </div>
             
                 {/* nouvelle couleur */}
-                <div className="embla__slide"> 
-                    <video
-                    className="embla__video"
-                    muted
-                    loop
-                    autoPlay
-                    >
-                    <source src="/videos/nouvellecouleur.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
+                <div className="embla__slide">
+                    <a href="https://youtu.be/2PM2P4K1qBk?list=RD2PM2P4K1qBk" target="_blank" rel="noopener noreferrer">
+                        <video
+                        className="embla__video"
+                        muted
+                        loop
+                        autoPlay
+                        >
+                        <source src="/videos/nouvellecouleur.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                        </video>
+                    </a>
                 </div>
 
                 {/* oignon */}
                 <div className="embla__slide"> 
-                    <video
-                    className="embla__video"
-                    muted
-                    loop
-                    autoPlay
-                    >
-                    <source src="/videos/oignon.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
+                    <a href="https://youtu.be/yKNas4OvNgI?si=VoU3Pkg3AmjCVm6B" target="_blank" rel="noopener noreferrer">
+                        <video
+                        className="embla__video"
+                        muted
+                        loop
+                        autoPlay
+                        >
+                        <source src="/videos/oignon.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                        </video>
+                    </a>
                 </div>
 
                 {/* rodman */}
-                <div className="embla__slide"> 
-                    <video
-                    className="embla__video"
-                    muted
-                    loop
-                    autoPlay
-                    >
-                    <source src="/videos/rodman.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
+                <div className="embla__slide">
+                    <a href="https://youtu.be/yKNas4OvNgI?si=VoU3Pkg3AmjCVm6B" target="_blank" rel="noopener noreferrer">
+                        <video
+                        className="embla__video"
+                        muted
+                        loop
+                        autoPlay
+                        >
+                        <source src="/videos/rodman.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                        </video>
+                    </a>
                 </div>
 
                 {/* studio gentil */}
-                <div className="embla__slide"> 
-                    <video
-                    className="embla__video"
-                    muted
-                    loop
-                    autoPlay
-                    >
-                    <source src="/videos/gentil.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
+                <div className="embla__slide">
+                    <a href="https://www.instagram.com/p/Cg4jiuwjgiQ/" target="_blank" rel="noopener noreferrer">
+                        <video
+                        className="embla__video"
+                        muted
+                        loop
+                        autoPlay
+                        >
+                        <source src="/videos/gentil.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                        </video>
+                    </a>
                 </div>
             </div>
         </div>
