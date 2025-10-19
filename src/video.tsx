d7a89326bdc1295d7b071a9e238507cfe42a2451
Cyb3rOguh <hugo_borel@me.com>
2025-10-19 16:40:10 +0200

    external links

--- src/video.tsx
+++ src/video.tsx
@@ -4,11 +4,11 @@ import Autoplay from 'embla-carousel-autoplay';
 import './video.css'; // Add custom styles for the carousel
 
 const slides = [
-  { src: '/videos/gagner.mp4', title: "gagner c'est bien perdre ca craint", year: "2025" },
-  { src: '/videos/nouvellecouleur.mp4', title: 'Nouvelle Couleur', year: "2024" },
-  { src: '/videos/oignon.mp4', title: 'Oignon', year: "2023" },
-  { src: '/videos/rodman.mp4', title: 'Rodman', year: "2022" },
-  { src: '/videos/gentil.mp4', title: 'Gentil', year: "2021" },
+  { src: '/videos/gagner.mp4', title: "gagner c'est bien perdre ca craint/Clip", year: "2025", url: "https://youtu.be/Ac3oQSA2Gv0" },
+  { src: '/videos/nouvellecouleur.mp4', title: 'nouvelle couleur/Clip', year: "2025", url: "https://youtu.be/2PM2P4K1qBk?list=RD2PM2P4K1qBk" },
+  { src: '/videos/oignon.mp4', title: 'le garcon oignon/Clip', year: "2025", url: "https://youtu.be/yKNas4OvNgI" },
+  { src: '/videos/rodman.mp4', title: 'Rodman/Clip', year: "2022", url: "https://youtu.be/iKLNpno3WVA" },
+  { src: '/videos/gentil.mp4', title: 'Studio Gentil/Dessin animé', year: "2023", url: "https://www.instagram.com/p/Cg4jiuwjgiQ/" },
 ];
 
 const VideoCarousel: React.FC = () => {
@@ -29,18 +29,20 @@ const VideoCarousel: React.FC = () => {
             className="embla__slide"
             key={index}
           >
-            <video
-              src={slide.src}
-              controls
-              muted
-              loop
-              playsInline
-              autoPlay
-            />
-            <div className="slide-info">
-              <span className="slide-title">{slide.title}</span>
-              <span className="slide-year">{slide.year}</span>
-            </div>
+            <a href={slide.url} target="_blank" rel="noopener noreferrer">
+              <video
+                src={slide.src}
+                controls
+                muted
+                loop
+                playsInline
+                autoPlay
+              />
+              <div className="slide-info">
+                <span className="slide-title">{slide.title}</span>
+                <span className="slide-year">{slide.year}</span>
+              </div>
+            </a>
           </div>
         ))}
       </div>
