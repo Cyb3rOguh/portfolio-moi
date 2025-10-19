4338b7cfcff7574e57066dc48e19c23939cf6257
Cyb3rOguh <hugo_borel@me.com>
2025-10-19 11:16:42 +0200

    titles

--- src/musique.tsx
+++ src/musique.tsx
@@ -20,37 +20,37 @@ const MusiqueCarousel = () => {
                 
                 <div className="embla-musique__slide">
                     <img src="/covers/bb.webp" alt="Zebre" />
-                    <span className="slide-title">Zebre</span>
+                    <span className="slide-title">Bébé/Album/2025</span>
                 </div>
                 
                 <div className="embla-musique__slide">
                     <img src="/covers/couleur.webp" alt="Another" />
-                    <span className="slide-title">Couleur</span>
+                    <span className="slide-title">Nouvelle Couleur/Morceau/2025</span>
                 </div>
 
                 <div className="embla-musique__slide">
                     <img src="/covers/gagner.webp" alt="Another" />
-                    <span className="slide-title">Gagner</span>
+                    <span className="slide-title">Gagner c'est bien perdre ça craint/Morceau/2025</span>
                 </div>
 
                 <div className="embla-musique__slide">
                     <img src="/covers/oignon.webp" alt="Another" />
-                    <span className="slide-title">Oignon</span>
+                    <span className="slide-title">Le garçon oignon/morceau/2025</span>
                 </div>
 
                 <div className="embla-musique__slide">
                     <img src="/covers/pandore.webp" alt="Another" />
-                    <span className="slide-title">Pandore</span>
+                    <span className="slide-title">Pandore/EP/2023</span>
                 </div>
 
                 <div className="embla-musique__slide">
                     <img src="/covers/morceau.webp" alt="Another" />
-                    <span className="slide-title">Morceau</span>
+                    <span className="slide-title">Le grand morçeau/morceau/2025</span>
                 </div>
 
                 <div className="embla-musique__slide">
                     <img src="/covers/saturn.webp" alt="Another" />
-                    <span className="slide-title">Saturn</span>
+                    <span className="slide-title">Lisa From Saturn/Album/2021</span>
                 </div>
 
             </div>            
