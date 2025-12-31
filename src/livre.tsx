4160911448884b425070e34bf911f05d403e1ebf
Cyb3rOguh <hugo_borel@me.com>
2025-12-31 22:16:49 +0100

    responsive design

--- src/livre.tsx
+++ src/livre.tsx
@@ -4,16 +4,18 @@ import "./Livre.css";
 const Livre = () => {
   return (
     <div className="livre-container">
-      {/* Conteneur pour l'image + le titre */}
-      <div className="image-title-container">
-        <img
-          src={`${process.env.PUBLIC_URL}/taper-packshot.webp`}
-          alt="Taper Packshot"
-          className="centered-image"
-        />
-        <span className="book-title">TAPER/Livre pour enfant/Dessin et ecriture</span>
+      {/* Image centrée */}
+      <img
+        src={`${process.env.PUBLIC_URL}/taper-packshot.webp`}
+        alt="Taper Packshot"
+        className="centered-image"
+      />
+      {/* Conteneur pour centrer le titre */}
+      <div className="title-container">
+        <span className="book-title">
+          TAPER/Livre pour enfants/Illustration et ecriture/2026
+        </span>
       </div>
-
       {/* Bouton à droite */}
       <button className="buy-button">
         Commander
