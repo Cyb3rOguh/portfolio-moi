5c382796ff57884c33fc077b891a6c8737b5506a
Cyb3rOguh <hugo_borel@me.com>
2025-12-31 19:21:42 +0100

    skank

--- src/livre.tsx
+++ src/livre.tsx
@@ -4,12 +4,16 @@ import "./Livre.css";
 const Livre = () => {
   return (
     <div className="livre-container">
-      {/* Image centrée */}
-      <img
-        src={`${process.env.PUBLIC_URL}/taper-packshot.webp`}
-        alt="Taper Packshot"
-        className="centered-image"
-      />
+      {/* Conteneur pour l'image + le titre */}
+      <div className="image-title-container">
+        <img
+          src={`${process.env.PUBLIC_URL}/taper-packshot.webp`}
+          alt="Taper Packshot"
+          className="centered-image"
+        />
+        <span className="book-title">TAPER/Livre pour enfant/Dessin et ecriture</span>
+      </div>
+
       {/* Bouton à droite */}
       <button className="buy-button">
         Commander
